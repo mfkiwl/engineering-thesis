b0fc2590d53b50c53c32246c8818560be64dcfb3
dgucik <danielgucik7@gmail.com>
2022-10-23 01:07:49 +0200

    update api code

--- api/ft601/ft601.c
+++ api/ft601/ft601.c
@@ -2,7 +2,7 @@
 #include <initguid.h>
 #include "FTD3XX.h"
 
-#define BUFFER_SIZE (8192)
+#define BUFFER_SIZE (8192*4)
 
 DEFINE_GUID(GUID_DEVINTERFACE_FOR_D3XX, 0xd1e8fe6a, 0xab75, 0x4d9e, 0x97, 0xd2, 0x6, 0xfa, 0x22, 0xc7, 0x73, 0x6c);
 
@@ -11,45 +11,48 @@ int main() {
     FT_HANDLE ftHandle;
     GUID DeviceGUID[2] = { 0 };
 
-    UCHAR acReadBuf[BUFFER_SIZE] = { 0xAA };
+    OVERLAPPED vOverlapped = { 0 };
+    UCHAR acReadBuf[BUFFER_SIZE] = { 0xFF };
     ULONG ulBytesRead = 0;
+    UCHAR ucPipeID = 0x82;
+    ULONG ulTimeoutInMs = 10000;
+    UINT32 uiData = 0;
 
     memcpy(&DeviceGUID[0], &GUID_DEVINTERFACE_FOR_D3XX, sizeof(GUID));
     ftStatus = FT_Create(&DeviceGUID[0], FT_OPEN_BY_GUID, &ftHandle);
-    if (FT_FAILED(ftStatus)) {
+
+    // Initialize resource for overlapped parameter
+    ftStatus = FT_InitializeOverlapped(ftHandle, &vOverlapped);
+
+    // Set pipe timeout
+    ftStatus = FT_SetPipeTimeout(ftHandle, ucPipeID, ulTimeoutInMs);
+
+    // Wait for anykey press
+    getch();
+
+    // Read data from pipe
+    ftStatus = FT_ReadPipe(ftHandle, ucPipeID, acReadBuf, sizeof(acReadBuf), &ulBytesRead, &vOverlapped);
+    if (ftStatus == FT_IO_PENDING)
+        ftStatus = FT_GetOverlappedResult(ftHandle, &vOverlapped, &ulBytesRead, TRUE);
+        
+    // Release resource for overlapped parameter
+    FT_ReleaseOverlapped(ftHandle, &vOverlapped);
+    
+    // Handle errors & data
+    if (ftStatus == FT_TIMEOUT) {
+        printf("Timeout has occured!");
         return FALSE;
     }
-
-    UINT32 failCtr = 0;
-    int loopctr = 0;
-    UINT32 uiDecValue = 0, uiDecValuePREV = 0;
-    ULONG ulBytesReadBuffer = 0;
-    while (loopctr <  500) {
-        loopctr++;
-        ulBytesReadBuffer = ulBytesRead;
-        ftStatus = FT_ReadPipe(ftHandle, 0x82, acReadBuf, sizeof(acReadBuf), &ulBytesRead, NULL);
-        if (FT_FAILED(ftStatus)) {
-            ulBytesRead = ulBytesReadBuffer;
-            break;
-        }
-
-        for (int i = 0; i < ulBytesRead; i++) {
-            if ((i + 1) % 4 == 0) {
-                uiDecValuePREV = uiDecValue;
-                uiDecValue = ((acReadBuf[i] << 24) | (acReadBuf[i - 1] << 16) | (acReadBuf[i - 2] << 8) | (acReadBuf[i - 3] << 0));
-                if ((uiDecValuePREV + 1) != uiDecValue) {
-                    failCtr++;
-                    printf("%d. <----------------\n", failCtr);         
-                }
-                printf("%d\n", uiDecValue);
-            }
-            else
-                continue;
+    else if (ftStatus != FT_OK) {
+        printf("Failed due to other errors!");
+        return FALSE;
+    }
+    else {
+        for (ULONG ulByteIndex = 3; ulByteIndex < ulBytesRead; ulByteIndex = ulByteIndex + 4) {
+            uiData = ((acReadBuf[ulByteIndex] << 24) | (acReadBuf[ulByteIndex - 1] << 16) | (acReadBuf[ulByteIndex - 2] << 8) | (acReadBuf[ulByteIndex - 3] << 0));
+            printf("%d\n", uiData);
         }
     }
-
-    printf("\n\nfails = %d\n", failCtr - 1);
-    printf("%d Bytes received\n\n", loopctr * ulBytesRead);
 
     FT_Close(ftHandle);
     return TRUE;
