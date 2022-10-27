4bce6cea8cef7e62c19644f04be34a18e0a94dfa
dgucik <danielgucik7@gmail.com>
2022-10-27 22:38:46 +0200

    create python api for usb data rx

--- api_python/ft601.py
+++ api_python/ft601.py
@@ -1,42 +1,31 @@
-from ftd3xx import *
-from ftd3xx.defines import *
-import time
+from ftd3xx import ftd3xx, defines
+import ctypes
+import msvcrt
 
-packets_to_read = 50
-
-def read_pipe_0x82(d3xx, packets):
-    data_out = []
-    bytes_read = 0
-    
-    # Read data from ft601 device
-    while(bytes_read < (packets * 4096)):
-        output = d3xx.readPipeEx(0x82, 4096)
-        bytes_read += output['bytesTransferred']
-        data_out += output['bytes'].encode('hex')
-
-    # Format data
-    data_out = [''.join(x) for x in zip(data_out[::2], data_out[1::2])]
-    data_out = [''.join(x) for x in zip(data_out[3::4], data_out[2::4], data_out[1::4], data_out[::4])]
-    data_out = [int(x, 16) for x in data_out]
-
-    return data_out, bytes_read
+bytes_to_read = 32768
+pipe_id = 0x82
+timeout_in_ms = 10000
 
 if __name__ == "__main__":
-    
+    data = ctypes.c_buffer(bytes_to_read)
 
-    ft_device = create(0, FT_OPEN_BY_GUID)
-    if ft_device is None:
-        print("ERROR: Please check if another D3XX application is open!")
+    ft_status = ftd3xx.create(0, defines.FT_OPEN_BY_GUID)
+
+    ft_status.setPipeTimeout(pipe_id, timeout_in_ms)
+
+    msvcrt.getch()
+
+    bytes_transferred = ft_status.readPipe(pipe_id, data, bytes_to_read)
+
+    if ftd3xx.getStrError(ft_status.getLastError()) == "FT_TIMEOUT":
+        print("Timeout has occured!")
         quit()
+    elif ftd3xx.getStrError(ft_status.getLastError()) != "FT_OK":
+        print("Failed due to other errors!")
+        quit()
+    else:
+        hex_byte_data_list = [byte.encode('hex') for byte in data]
+        print(hex_byte_data_list)
+        print(bytes_transferred)
 
-    start_time = time.time()
-    data, bytes_read = read_pipe_0x82(ft_device, packets_to_read)
-    stop_time = time.time()
-    
-    print(data)
-    print(bytes_read)
-    print("-----%s seconds ----", (stop_time- start_time))
-
-    ft_device.close()
-
-    
+    ft_status.close()
